12b52c9a0a7d151b76a2d882e051108421675066
Nikhita-Vanam <180030469cse@gmail.com>
2022-06-15 15:54:07 +0530

    first commit

---
+++ src/test/java/com/sapient/asde/service/PersonTest.java
@@ -0,0 +1,76 @@
+package com.sapient.asde.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+    private Person person;
+    @BeforeEach
+    void setUp() {
+        person=new Person(123456789012L,"Nikhita", LocalDate.of(2000,12,23),"Vijayawada",9492390783L);
+    }
+
+    @AfterEach
+    void tearDown() {
+        person=null;
+    }
+
+    @Test
+    void getAdharCardValidData() {
+        assertEquals(123456789012L,person.getAdharCard());
+    }
+    @Test
+    void getAdharCardInvalidData() {
+        assertNotEquals(12346789012L,person.getAdharCard());
+    }
+
+    @Test
+    void setAdharCard() {
+    }
+
+    @Test
+    void getNameValidData() {
+        assertEquals("Nikhita",person.getName());
+    }
+
+    @Test
+    void getNameInvalidData() {
+        assertNotEquals("Nikhi",person.getName());
+    }
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getBirthdate() {
+    }
+
+    @Test
+    void setBirthdate() {
+    }
+
+    @Test
+    void getAddress() {
+    }
+
+    @Test
+    void setAddress() {
+    }
+
+    @Test
+    void getMobile() {
+    }
+
+    @Test
+    void setMobile() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
---
+++ src/test/java/com/sapient/asde/suit/SuiteClassTest.java
@@ -0,0 +1,16 @@
+package com.sapient.asde.suit;
+
+import com.sapient.asde.service.HelloWorldLatestTest;
+import com.sapient.asde.service.HelloWorldTest;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+//@SelectClasses({com.sapient.asde.service.HelloWorldLatestTest.class,
+//        com.sapient.asde.service.HelloWorldTest.class})
+
+@SelectPackages("com.sapient.asde.service")
+public class SuiteClassTest {
+}
+
